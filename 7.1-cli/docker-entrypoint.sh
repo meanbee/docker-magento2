5eee04b5b181ce32cce2d930263fd9fd6f79a8f8
Nick <nick@nicksays.co.uk>
2017-09-18 11:54:12 +0100

    Implement image templating (#102)
    * Use templating to build the PHP images
    
    * Use PHP 7.0 in the example Docker Compose files
    
    We can't install a version of Magento that's compatible with 7.1 at the moment
    
    * Fix CS issues in the Builder script
    
    * Add instructions on building

--- 7.1-cli/docker-entrypoint.sh
+++ 7.1-cli/docker-entrypoint.sh
@@ -31,6 +31,7 @@ fi
 mkdir -p $MAGENTO_ROOT
 chown www-data:www-data $MAGENTO_ROOT
 
+
 CRON_LOG=/var/log/cron.log
 
 # Setup Magento cron
@@ -43,19 +44,21 @@ touch $CRON_LOG
 echo "cron.* $CRON_LOG" > /etc/rsyslog.d/cron.conf
 service rsyslog start
 
+
+
 # Configure Sendmail if required
 if [ "$ENABLE_SENDMAIL" == "true" ]; then
     /etc/init.d/sendmail start
 fi
 
-
-# Configure PHP
+# Substitute in php.ini values
 [ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s/!PHP_MEMORY_LIMIT!/${PHP_MEMORY_LIMIT}/" /usr/local/etc/php/conf.d/zz-magento.ini
 [ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s/!UPLOAD_MAX_FILESIZE!/${UPLOAD_MAX_FILESIZE}/" /usr/local/etc/php/conf.d/zz-magento.ini
 
 [ "$PHP_ENABLE_XDEBUG" = "true" ] && \
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
+
 
 # Configure composer
 [ ! -z "${COMPOSER_GITHUB_TOKEN}" ] && \
@@ -64,8 +67,10 @@ fi
 [ ! -z "${COMPOSER_MAGENTO_USERNAME}" ] && \
     composer config --global http-basic.repo.magento.com \
         $COMPOSER_MAGENTO_USERNAME $COMPOSER_MAGENTO_PASSWORD
-        
+
 [ ! -z "${COMPOSER_BITBUCKET_KEY}" ] && [ ! -z "${COMPOSER_BITBUCKET_SECRET}" ] && \
-    composer config --global bitbucket-oauth.bitbucket.org $COMPOSER_BITBUCKET_KEY $COMPOSER_BITBUCKET_SECRET        
+    composer config --global bitbucket-oauth.bitbucket.org $COMPOSER_BITBUCKET_KEY $COMPOSER_BITBUCKET_SECRET
+
 
 exec "$@"
+
--- 7.1-fpm/docker-entrypoint.sh
+++ 7.1-fpm/docker-entrypoint.sh
@@ -27,19 +27,29 @@ if [[ "$UPDATE_UID_GID" = "true" ]]; then
     groupmod -g $DOCKER_GID www-data
 fi
 
+# Ensure our Magento directory exists
+mkdir -p $MAGENTO_ROOT
+chown www-data:www-data $MAGENTO_ROOT
+
+
+
 # Configure Sendmail if required
 if [ "$ENABLE_SENDMAIL" == "true" ]; then
     /etc/init.d/sendmail start
 fi
 
-# Configure PHP
+# Substitute in php.ini values
 [ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s/!PHP_MEMORY_LIMIT!/${PHP_MEMORY_LIMIT}/" /usr/local/etc/php/conf.d/zz-magento.ini
+[ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s/!UPLOAD_MAX_FILESIZE!/${UPLOAD_MAX_FILESIZE}/" /usr/local/etc/php/conf.d/zz-magento.ini
 
 [ "$PHP_ENABLE_XDEBUG" = "true" ] && \
     docker-php-ext-enable xdebug && \
     echo "Xdebug is enabled"
 
+
 # Configure PHP-FPM
 [ ! -z "${MAGENTO_RUN_MODE}" ] && sed -i "s/!MAGENTO_RUN_MODE!/${MAGENTO_RUN_MODE}/" /usr/local/etc/php-fpm.conf
 
+
 exec "$@"
+
