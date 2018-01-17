5deeb780d9db11f3046f8ca2c4b605331668a3ea
Nick Jones <nick@nicksays.co.uk>
2018-01-17 10:23:05 +0000

    Remove check for ioncube

--- test/test.php
+++ test/test.php
@@ -39,18 +39,9 @@ $required_extensions = array(
   'pdo_mysql',
   'simplexml',
   'xsl',
-  'IonCube Loader',
   'zip',
   'intl'
 );
-
-// remove IoCube Loader from required extensions if php version is greater than/equal to 7.1
-if (version_compare(phpversion(), '7.1.0') >= 0) {
-    $key = array_search('IonCube Loader', $required_extensions);
-    if (false !== $key) {
-        unset($required_extensions[$key]);
-    }
-}
 
 foreach ($required_extensions as $extension) {
   if (!extension_loaded($extension)) {
